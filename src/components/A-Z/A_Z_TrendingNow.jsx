9bf3d996a957ab52811b6795a80a0b6c54a88133
PawelPavo <Pawelebllc@gmail.com>
2024-09-15 13:54:04 -0400

    A-Z carousel update

--- src/components/A-Z/A_Z_TrendingNow.jsx
+++ src/components/A-Z/A_Z_TrendingNow.jsx
@@ -34,27 +34,36 @@ function CustomSlide(props) {
     const { index, ...otherProps } = props;
     return (
             <div {...otherProps}>
-            <div class="card mx-2 p-2">
+            <div class="card mx-2 p-2 bg-light" style={{minHeight:"200px"}}>
                 <div class="row justify-content-center">
-                    <div class="col-lg-3 my-auto">
+                    <div class="col-3 my-auto">
                         <div class="row justify-content-center">
                             <img src="https://www.lifeextension.com/-/media/lifeextension/products/small/01758.png"
                                 class="img-fluid rounded-start" alt="..." style={{ width: "75px" }} />
                         </div>
                     </div>
-                    <div class="col-lg-8 m-0 p-0">
+                    <div class="col-8 m-0 p-0">
                         <div class="card-body">
                             <a href="#" className="add_unit_name fs-6">
-                            <div class="" style={{minHeight:"50px"}}>{props.title}</div>
+                            <div className="row post-desc" style={{height:"55px"}}>{props.title}</div>
                             </a>
-                            <div class="row card-text text-end justify-content-between px-3">
-                                <div class="my-auto fs-5 text-center mb-2"><span class="fw-bold text-danger">$5.00</span> | <span
+                            <div class="row justify-content-between px-3">
+                                <div class="my-auto text-center mb-2"><span class="fw-bold text-danger">$5.00</span> | <span
                                     class="text-decoration-line-through" style={{ fontSize: "16px" }}>$9.45</span>
                                 </div>
-                                <button class="btn btn-orange-white btn-sm" onClick={props.addToCart}>Add To Cart</button>
+                                <button class="btn btn-orange-white btn-sm fw-bold" onClick={props.addToCart}>Add To Cart</button>
+                                
                             </div>
+                            
                         </div>
+                        
                     </div>
+
+                </div>
+                <div class="card-footer bg-white border-0 p-0">
+                    {props.view==1&&<div className="border rounded-5 text-center p-1 shadow-sm bg-warning-subtle text-small">Viewed 784 times in the last 6 hours</div>}
+                    {props.purchase==1&&<div className="border rounded-5 text-center p-1 shadow-sm bg-primary-subtle text-small">Last purchased Nov 24, 2023</div>}
+                    {props.current==1&&<div className="border rounded-5 text-center p-1 shadow-sm bg-info-subtle text-small">Currently in 127 carts</div>}
                 </div>
             </div>
         </div>
@@ -79,7 +88,6 @@ const ReactCarousel = (props) => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-        initialSlide: 2,
         dots: true,
         responsive: [
             {
@@ -130,12 +138,12 @@ const ReactCarousel = (props) => {
                         sliderRef = slider;
                     }}
                     {...settings}>
-                    <CustomSlide index={1} title={"Advanced Lipid Control"}/>
-                    <CustomSlide index={2} title={"Arthro-Immune Joint Support"}/>
-                    <CustomSlide index={3} title={"Bioactive Milk Peptides"}/>
-                    <CustomSlide index={4} title={"Black Cumin Seed Oil, 60 softgels"}/>
-                    <CustomSlide index={5} title={"Bone Restore Elite with Super Potent K2"}/>
-                    <CustomSlide index={6} title={"Standardized Cistanche"}/>
+                    <CustomSlide index={1} title={"ArthroMax® Advanced with NT2 Collagen™"} view={1} current={0} purchase={0}/>
+                    <CustomSlide index={2} title={"Arthro-Immune Joint Support"} view={0} current={1} purchase={0}/>
+                    <CustomSlide index={3} title={"Bioactive Milk Peptides"} view={0} current={0} purchase={1}/>
+                    <CustomSlide index={4} title={"Black Cumin Seed Oil"} view={1} current={0} purchase={0}/>
+                    <CustomSlide index={5} title={"Bone Restore Elite with Super Potent K2"} view={0} current={1} purchase={0}/>
+                    <CustomSlide index={6} title={"Standardized Cistanche"} view={0} current={1} purchase={0}/>
                 </Slider>
                 </div>
                 <div className="col-1 my-auto d-none d-lg-block text-center">
